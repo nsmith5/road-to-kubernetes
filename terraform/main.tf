99abb2313e72ef61dfcce120625221b5a9e8645c
Nathan Smith <nathan@nfsmith.ca>
2020-11-05 21:14:13 -0800

    Terraform for cloudrun and registry

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+provider "google" {
+  project = "cloudrun-284100"
+}
+
+resource "google_project_service" "run" {
+  service = "run.googleapis.com"
+}
+
+resource "google_container_registry" "registry" {
+  project  = "cloudrun-284100"
+  location = "US"
+}
+
+resource "google_cloud_run_service" "my-service" {
+  name     = "road-to-kubernetes"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us.gcr.io/cloudrun-284100/road-to-kubernetes:${var.image_tag}"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  depends_on = [google_project_service.run]
+}
+
+resource "google_cloud_run_service_iam_member" "allUsers" {
+  service  = google_cloud_run_service.my-service.name
+  location = google_cloud_run_service.my-service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "image_tag" {
+  description = "Image tag to deploy"
+}
