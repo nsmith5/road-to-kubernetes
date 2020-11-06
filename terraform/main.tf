78a84e6ca2b4e42bf5e7046acc9befd3fe9b1e8f
Nathan Smith <nathan@nfsmith.ca>
2020-11-05 22:47:20 -0800

    Add terraform deploy to CI pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,34 @@ resource "google_project_service" "run" {
   service = "run.googleapis.com"
 }
 
+resource "google_project_service" "redis" {
+  service = "redis.googleapis.com"
+}
+
+resource "google_project_service" "vpcaccess" {
+  service = "vpcaccess.googleapis.com"
+}
+
 resource "google_container_registry" "registry" {
   project  = "cloudrun-284100"
   location = "US"
+}
+
+resource "google_redis_instance" "cache" {
+  name           = "cache"
+  memory_size_gb = 1
+  region         = "us-central1"
+
+  depends_on = [google_project_service.redis]
+}
+
+resource "google_vpc_access_connector" "connector" {
+    name          = "vpcconn"
+    region        = "us-central1"
+    ip_cidr_range = "10.8.0.0/28"
+    network       = "default"
+
+    depends_on = [google_project_service.vpcaccess]
 }
 
 resource "google_cloud_run_service" "my-service" {
@@ -20,9 +45,23 @@ resource "google_cloud_run_service" "my-service" {
       containers {
         image = "us.gcr.io/cloudrun-284100/road-to-kubernetes:${var.image_tag}"
         env {
-          name = "BIND_PORT"
+          name  = "BIND_PORT"
           value = "8080"
         }
+        env {
+          name  = "REDIS_HOST"
+          value = google_redis_instance.cache.host
+        }
+        env {
+          name  = "REDIS_PORT"
+          value = google_redis_instance.cache.port
+        }
+      }
+    }
+    
+    metadata {
+      annotations = {
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
       }
     }
   }
@@ -32,7 +71,10 @@ resource "google_cloud_run_service" "my-service" {
     latest_revision = true
   }
 
-  depends_on = [google_project_service.run]
+  depends_on = [
+    google_project_service.run,
+    google_vpc_access_connector.connector
+  ]
 }
 
 resource "google_cloud_run_service_iam_member" "allUsers" {
